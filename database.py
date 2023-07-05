a6ebfa40a85c53f26edf87a1a0a287d4204c577e
bkarthik <bkarthik338@gmail.com>
2023-07-05 17:31:49 +0530

    adding database configuration file

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
-#datatbase connection file
-
-from pymongo import MongoClient
-from dotenv import load_dotenv
+# datatbase connection file
 import os
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+
 # Load environment variables from .env file
 load_dotenv()
-db_host = os.environ.get('DB_HOST')
-db_port = os.environ.get('DB_PORT')
-db_name = os.environ.get('DB_NAME')
+db_host = os.environ.get("DB_HOST")
+db_port = os.environ.get("DB_PORT")
+db_name = os.environ.get("DB_NAME")
 client = MongoClient(f"mongodb://{db_host}:{db_port}/")
 db = client[db_name]
