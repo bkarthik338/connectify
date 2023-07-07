e7531bd86f28d769fc0f14b2e2ab3d7dd23f2e79
bkarthik <bkarthik338@gmail.com>
2023-07-07 14:22:49 +0530

    changes made for db connection for github runner

--- database.py
+++ database.py
@@ -11,4 +11,4 @@ db_host = os.environ.get("DB_HOST")
 db_port = os.environ.get("DB_PORT")
 db_name = os.environ.get("DB_NAME")
 client = MongoClient("mongodb://mongo:27017/")
-db = client[db_name]
+db = client["test-connectify-db"]
