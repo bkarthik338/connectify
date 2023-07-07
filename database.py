50bb12791316e587f95ed94db605d4edaf823dca
bkarthik <bkarthik338@gmail.com>
2023-07-07 14:19:30 +0530

    changes made for db connection for github runner

--- database.py
+++ database.py
@@ -10,5 +10,5 @@ load_dotenv()
 db_host = os.environ.get("DB_HOST")
 db_port = os.environ.get("DB_PORT")
 db_name = os.environ.get("DB_NAME")
-client = MongoClient("mongodb://mongo-service:27017/")
+client = MongoClient("mongodb://mongo:27017/")
 db = client[db_name]
