69e24fc96b2ec8e47dff2347f760de8895342a57
bkarthik <bkarthik338@gmail.com>
2023-07-08 10:55:37 +0530

    testing ci/cd

--- database.py
+++ database.py
@@ -10,5 +10,5 @@ load_dotenv()
 db_host = os.environ.get("DB_HOST")
 db_port = os.environ.get("DB_PORT")
 db_name = os.environ.get("DB_NAME")
-client = MongoClient("mongodb://mongo:27017/")
+client = MongoClient("mongodb://127.0.0.1:27017/")
 db = client["test-connectify-db"]
