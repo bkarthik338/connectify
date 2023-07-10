6293789b1adb6c2117c94c6d1489db610ea77f5b
bkarthik <bkarthik338@gmail.com>
2023-07-10 11:28:32 +0530

    changed the folder structure Request/Response Schema to be kept in models folder

---
+++ models/user_model.py
@@ -0,0 +1,39 @@
+import strawberry
+
+
+# User Model
+@strawberry.type
+class User:
+    id: str
+    username: str
+    email: str
+    password: str = None
+
+
+# SignUP API Response
+@strawberry.type
+class CreateUserResponse:
+    success: bool
+    msg: str
+
+
+# Delete User API Response
+@strawberry.type
+class DeleteUserResponse:
+    success: bool
+    msg: str
+
+
+# Getting User API Response
+@strawberry.type
+class GetUserResponse:
+    data: User
+    success: bool
+
+
+# Getting User API Failure Response
+# (Invalid Request/Parameter)
+@strawberry.type
+class GetUserFailureResponse:
+    error: str
+    success: bool
