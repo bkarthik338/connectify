72d191a3677ff4215b4c92370831aa43fd9ac6ae
bkarthik <bkarthik338@gmail.com>
2023-07-13 16:01:57 +0530

    adding get-single-tweet api and testcase for that

--- models/tweet_model.py
+++ models/tweet_model.py
@@ -1,14 +1,33 @@
 from typing import List
+from typing import Optional
 
 import strawberry
+from bson import ObjectId
 
 
 # Get Tweets Response Model
 @strawberry.type
 class TweetModel:
-    id: str
-    description: str
-    hashtags: str = None
+    id: Optional[str] = None
+    description: Optional[str] = None
+    hashtags: Optional[str] = None
+
+    @classmethod
+    def from_dict(cls, data):
+        instance = cls()
+        for key, value in data.items():
+            if isinstance(value, ObjectId):
+                setattr(instance, key, value)
+                continue
+            setattr(instance, key, value)
+        return instance
+
+
+# SingleTweetModel
+@strawberry.type
+class SingleTweetModel:
+    tweet: TweetModel
+    success: bool
 
 
 @strawberry.type
--- query/tweet_query.py
+++ query/tweet_query.py
@@ -5,6 +5,7 @@ from bson import ObjectId
 
 from database import db
 from models.tweet_model import ListTweetModel
+from models.tweet_model import SingleTweetModel
 from models.tweet_model import TweetModel
 from models.user_model import GeneralResponse
 from utility.user_utility import verify_user_token
@@ -36,3 +37,21 @@ class TweetQuery:
             for tweet in response
         ]
         return ListTweetModel(success=True, tweets=tweets)
+
+    @strawberry.field
+    def get_single_tweet(
+        self, info, token: str, tweetId: str
+    ) -> Union[GeneralResponse, SingleTweetModel]:
+        user_data = verify_user_token(token=token)
+        if not user_data["success"]:
+            return GeneralResponse(
+                msg=f"Authentication Failed: {user_data['response']}",
+                success=False,
+            )
+        response = tweet_collection.find_one({"_id": ObjectId(tweetId)})
+        if not response:
+            return GeneralResponse(msg="Tweet Not Found", success=False)
+        response["id"] = response.pop("_id")
+        return SingleTweetModel(
+            tweet=TweetModel().from_dict(response), success=True
+        )
